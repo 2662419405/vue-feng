a8dc1cbefa946ff8486f759b0f954c3bd5ae3361
孙航 <2662419405@qq.com>
2020-06-16 08:37:25 +0800

    test: 测试图片版本

--- src/plugins/antd-vue.js
+++ src/plugins/antd-vue.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import AntDesign from 'ant-design-vue'
+import VueLazyload from 'vue-lazyload'
 
 if (process.env.NODE_ENV !== 'production') {
   require('ant-design-vue/dist/antd.min.css')
   Vue.use(AntDesign)
 }
+
+Vue.use(VueLazyload, {
+  throttleWait: 0,
+})
